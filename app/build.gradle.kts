2a367b018c42a886c996fa3050f9f63be224d1d4
Bogusz Pawłowski <bogusz.pawlowski@gmail.com>
2021-03-24 21:59:53 +0100

    Bumped all dependencies, updated gradle files (#3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,28 @@
+import org.ajoberstar.grgit.Grgit
+
 plugins {
   id(Android.ApplicationPluginId)
   kotlin(Kotlin.AndroidPluginId)
   id("common-android-plugin")
 }
 
+val commitsCount = Grgit.open(mapOf("dir" to rootDir)).log().size
+
+android {
+  defaultConfig {
+    versionCode = commitsCount
+  }
+}
+
 dependencies {
-  implementation(Kotlin.StdLibJdk7)
+  implementation(Kotlin.StdLib)
 
   implementation(Material.Core)
+
+  implementation(Compose.Runtime)
 
   implementation(AndroidX.AppCompat)
   implementation(AndroidX.ConstraintLayout)
 
-  debugImplementation(Hyperion.Core)
-  debugImplementation(Hyperion.Crash)
-  debugImplementation(Hyperion.Timber)
-  debugImplementation(Hyperion.SharedPrefs)
+  debugImplementation(Debug.FoQA)
 }
